7cfe96052f81368be887624cb3db0e7cac004eae
Tyler-Losinski <tyler.a.losinski@ndsu.edu>
2017-02-19 16:57:55 -0600

    Created the Excel Parser

--- Data61/Data61/Program.cs
+++ Data61/Data61/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data61.Excel_Parser;
+using System.Data;
 
 namespace Data61
 {
@@ -10,6 +12,26 @@ namespace Data61
     {
         static void Main(string[] args)
         {
+            Console.Write("Started!");
+            ExcelParser excelData = new ExcelParser();
+
+            //Prints every csv files's cell with the column header it's in
+            foreach (DataTable dataTable in excelData.DataTables)
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        Console.Write("Item: ");
+                        Console.Write(column.ColumnName);
+                        Console.Write(" ");
+                        Console.WriteLine(row[column]);
+                    }
+                }
+            }
+
+            //Keep the application from stoping so we can test
+            Console.ReadKey();
         }
     }
 }
