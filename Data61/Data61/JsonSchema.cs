99e8fb225e70f7d034330584efe44c23188672ac
Unknown <tyler.a.losinski@ndsu.edu>
2017-02-20 11:53:28 -0600

    Created the JSON parser and its associated Data Structure
    Beat you to it Adam :P

---
+++ Data61/Data61/JsonSchema.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace Data61
+{
+    /// <summary>
+    /// Object to describe the DataSets 
+    /// </summary>
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Path { get; set; }
+    }
+
+    /// <summary>
+    /// Used to show the info of the Source DataTables
+    /// </summary>
+    public class Source
+    {
+        public Data Data { get; set; }
+    }
+
+    /// <summary>
+    /// Used to show the info of the Target DataTables
+    /// </summary>
+    public class Target
+    {
+        public Data Data { get; set; }
+    }
+
+    /// <summary>
+    /// Used to create Dataset objects
+    /// </summary>
+    public class Datasets
+    {
+        public List<Source> Source { get; set; }
+        public List<Target> Target { get; set; }
+    }
+
+    /// <summary>
+    /// Used to generate mapping objects
+    /// </summary>
+    public class Mappings
+    {
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public string Operation { get; set; }
+    }
+
+    /// <summary>
+    /// Main object 
+    /// </summary>
+    class JsonSchema
+    {
+        public Datasets Datasets { get; set; }
+
+        public List<Mappings> Mappings { get; set; }
+    }
+}
--- Data61/Data61/Program.cs
+++ Data61/Data61/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Data61.Excel_Parser;
 using System.Data;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Data61
 {
@@ -13,30 +12,56 @@ namespace Data61
         static void Main(string[] args)
         {
             Console.WriteLine("Started!");
-            ExcelParser excelData = new ExcelParser();
 
-            ////Prints every csv files's cell with the column header it's in
-            //foreach (DataTable dataTable in excelData.DataTables.Values)
-            //{
-            //    foreach (DataRow row in dataTable.Rows)
-            //    {
-            //        foreach (DataColumn column in dataTable.Columns)
-            //        {
-            //            Console.Write("Item: ");
-            //            Console.Write(column.ColumnName);
-            //            Console.Write(" ");
-            //            Console.WriteLine(row[column]);
-            //        }
-            //    }
-            //}
+            //Uncomment if you want to test
+            //JsonExampleTest();
+            //ExcelExampleTest();
 
-            foreach(string key in excelData.DataTables.Keys)
-            {
-                Console.WriteLine(key);
-            }
 
             //Keep the application from stoping so we can test
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Test how to pull the data from the json file
+        /// </summary>
+        public static void JsonExampleTest()
+        {
+            //Get the json file as a string
+            string stringJson = JObject.Parse(File.ReadAllText(@"..\..\ParseThese\integration.json")).ToString();
+            //Create a C# object from it
+            JsonSchema jsonObject = JsonConvert.DeserializeObject<JsonSchema>(stringJson);
+
+            //Example of how get the data from the json object
+            foreach (Source data in jsonObject.Datasets.Source)
+            {
+                Console.WriteLine(data.Data.Name);
+            }
+
+        }
+
+        /// <summary>
+        /// Test how to get the data from the excel file
+        /// </summary>
+        public static void ExcelExampleTest()
+        {
+            //Gets the excel data
+            ExcelParser excelData = new ExcelParser();
+
+            //Prints every csv files's cell with the column header it's in
+            foreach (DataTable dataTable in excelData.DataTables.Values)
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        Console.Write("Item: ");
+                        Console.Write(column.ColumnName);
+                        Console.Write(" ");
+                        Console.WriteLine(row[column]);
+                    }
+                }
+            }
         }
     }
 }
