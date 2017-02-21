5fbfe2de1a201a9f29565a9acce7b0429f028be5
DiamondIceNS <fawfulcopter@gmail.com>
2017-02-21 06:44:03 -0600

    Added the transformation and load pieces Wrote a new modular CSV parser so I could take better advantage of it

---
+++ Data61/Data61/DataFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Data61
+{
+    /// <summary>
+    /// Represents a connection to a CSV file
+    /// </summary>
+    class DataFile
+    {
+        readonly string _baseDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"..\..\ParseThese"));
+        private readonly OleDbConnection _odbConnection;
+        private OleDbCommand _odbCommand;
+        private OleDbDataReader _odbReader;
+
+        public string Name { get; }
+        public string Path { get; }
+
+        public DataFile(string name, string path)
+        {
+            Name = name;
+            Path = path;
+            try
+            {
+                _odbConnection =
+                    new OleDbConnection(
+                        $@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={_baseDirectory};Extended properties=""Text;HDR=Yes""");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public void SetQuery(string sql)
+        {
+            _odbConnection.Open();
+            _odbCommand = new OleDbCommand(sql, _odbConnection);
+            _odbReader = _odbCommand.ExecuteReader();
+        }
+
+        public bool Next()
+        {
+            if (_odbReader.Read()) return true;
+            _odbReader.Close();
+            _odbConnection.Close();
+            return false;
+        }
+
+        public string GetString(int ordinal)
+        {
+            return _odbReader.GetValue(ordinal).ToString();
+        }
+
+        public int Push(string sql)
+        {
+            _odbConnection.Open();
+            OleDbCommand insertCommand = new OleDbCommand(sql, _odbConnection);
+            int result = insertCommand.ExecuteNonQuery();
+            _odbConnection.Close();
+            return result;
+        }
+
+    }
+}
--- Data61/Data61/Program.cs
+++ Data61/Data61/Program.cs
@@ -1,17 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using Data61.Excel_Parser;
 using System.Data;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace Data61
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Started!");
+
+            //Read in JSON file
+            string stringJson = JObject.Parse(File.ReadAllText(@"..\..\ParseThese\integration.json")).ToString();
+            //string stringJson = JObject.Parse(File.ReadAllText(@"..\..\ParseThese\integration2.json")).ToString(); // Try using this other integration file!
+            JsonSchema transform = JsonConvert.DeserializeObject<JsonSchema>(stringJson);
+
+            //Create DataFile objects for source(s) and target
+            List<DataFile> sourceDataFiles = transform.Datasets.Source.Select(s => new DataFile(transform.Datasets.Source[0].Data.Name, transform.Datasets.Source[0].Data.Path)).ToList();
+            DataFile targetDataFile = new DataFile(transform.Datasets.Target[0].Data.Name, transform.Datasets.Target[0].Data.Path);
+
+            // Iterate over the source files
+            foreach (DataFile df in sourceDataFiles)
+            {
+                // Store all schema transformations in an ordered Dictionary
+                var schemaTranformations = new OrderedDictionary();
+                foreach (var m in transform.Mappings)
+                    if (m.Source.StartsWith(df.Name))
+                        schemaTranformations.Add(m.Target.Substring(targetDataFile.Name.Length + 1), m.Source.Substring(df.Name.Length + 1));
+
+                // Convert the keys and values to arrays so we can work with them
+                var keys = new string[schemaTranformations.Count];
+                schemaTranformations.Keys.CopyTo(keys, 0);
+                var values = new string[schemaTranformations.Count];
+                schemaTranformations.Values.CopyTo(values, 0);
+
+                // Set the select query to only pick up the columns we care about
+                string sql = $"SELECT {string.Join(",", values)} FROM [{df.Path}]";
+                df.SetQuery(sql);
+
+                // Read out a single row from the source dataset at a time, transform the data, and push it to the target
+                while (df.Next())
+                {
+                    var queryValues = new string[schemaTranformations.Count];
+                    for (int i = 0; i < schemaTranformations.Count; i++)
+                        queryValues[i] = df.GetString(i).Replace("'", "''");
+
+                    string sql2 = $"INSERT INTO [{targetDataFile.Path}] ({string.Join(",", keys)}) VALUES ('{string.Join("','", queryValues)}')";
+                    targetDataFile.Push(sql2);
+                }
+            }
+
+            Console.WriteLine("Done!");
 
             //Uncomment if you want to test
             //JsonExampleTest();
