15092a722eceffb77b605aa3b15d274e2f89e30e
Tyler-Losinski <tyler.a.losinski@ndsu.edu>
2017-02-28 11:40:10 -0600

    Cleaned up some useless code

--- Data61/Data61/Program.cs
+++ Data61/Data61/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using Data61.Excel_Parser;
 using System.Data;
 using Newtonsoft.Json.Linq;
 using System.IO;
@@ -58,55 +57,8 @@ namespace Data61
 
             Console.WriteLine("Done!");
 
-            //Uncomment if you want to test
-            //JsonExampleTest();
-            //ExcelExampleTest();
-
-
             //Keep the application from stoping so we can test
             Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Test how to pull the data from the json file
-        /// </summary>
-        public static void JsonExampleTest()
-        {
-            //Get the json file as a string
-            string stringJson = JObject.Parse(File.ReadAllText(@"..\..\ParseThese\integration.json")).ToString();
-            //Create a C# object from it
-            JsonSchema jsonObject = JsonConvert.DeserializeObject<JsonSchema>(stringJson);
-
-            //Example of how get the data from the json object
-            foreach (Source data in jsonObject.Datasets.Source)
-            {
-                Console.WriteLine(data.Data.Name);
-            }
-
-        }
-
-        /// <summary>
-        /// Test how to get the data from the excel file
-        /// </summary>
-        public static void ExcelExampleTest()
-        {
-            //Gets the excel data
-            ExcelParser excelData = new ExcelParser();
-
-            //Prints every csv files's cell with the column header it's in
-            foreach (DataTable dataTable in excelData.DataTables.Values)
-            {
-                foreach (DataRow row in dataTable.Rows)
-                {
-                    foreach (DataColumn column in dataTable.Columns)
-                    {
-                        Console.Write("Item: ");
-                        Console.Write(column.ColumnName);
-                        Console.Write(" ");
-                        Console.WriteLine(row[column]);
-                    }
-                }
-            }
         }
     }
 }
